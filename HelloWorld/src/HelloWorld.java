c60b42bd06ae5312a5f5f95c016959542f467b59
liuxia <liuxia@790c8510-c5db-6745-b320-83eac29dcb59>
2012-11-28 11:47:25 +0000

    git-svn-id: https://localhost:8443/svn/jenkins@8 790c8510-c5db-6745-b320-83eac29dcb59

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,7 @@
- public class HelloWorld
- { public static void main (String args[])
-    { 
-	   System.out.println("Hello World!!!"); 
-	}
- } 
+package oata;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
