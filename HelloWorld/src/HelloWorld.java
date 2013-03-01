50ddcc3f9ac54fa59d97da1c06fd7e1c2741ff42
xiaoxia <lx_jia@yahoo.cn>
2013-03-01 15:08:03 +0800

    tianjia2getanhao

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
          printf("%d,Thank you");
+	 printf("%d,Thank you!!");
 	 }
 }
